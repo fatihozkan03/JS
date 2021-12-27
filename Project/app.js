c4fcf15516f253d715f475277a6e18d8bfd4e4aa
fatihozkan03 <69695571+fatihozkan03@users.noreply.github.com>
2021-12-27 18:10:27 +0300

    Update app.js

--- Project/app.js
+++ Project/app.js
@@ -94,6 +94,15 @@ function handleGameSpeed() {
             resetAllAnimations()
         }, timeoutLength )
     }
+    if (doRest) {
+
+        hole.addEventListener('animationiteration',()=>{
+            resetAllAnimations()
+        })
+        block.addEventListener('animationiteration', () => {
+           resetAllAnimations()
+        })
+}
 }
 
 function handleCharacterAnimation( direction ) {
